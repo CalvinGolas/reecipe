0df3ae898e32142380307ddd521f5411e6fa60e1
XaosTheMad <jlevier@hotmail.com>
2021-08-09 18:05:55 -0600

    refactored for readability and added json output

--- Webscraping_Reecipe.py
+++ Webscraping_Reecipe.py
@@ -4,63 +4,94 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import re
+import json
 
 option = webdriver.ChromeOptions()
 option.add_argument("-incognito")
 
-browser = webdriver.Chrome(executable_path= '/Users/Reece/PycharmProjects/Reecipe/Webscraper/chromedriver.exe', options=option)
-browser.get("https://www.allrecipes.com/recipes/76/appetizers-and-snacks/")
+file_output_location = "D:\,,,\Owner\Documents\Reecepy\jsons\jsontest.json"
 
+browser = webdriver.Chrome(executable_path= 'D:\,,,\Owner\Documents\Reecepy\chromedriver_win32\chromedriver.exe', options=option)
+browser.get("https://www.allrecipes.com/recipes/76/appetizers-and-snacks/")
 timeout = 30
+# try:
+#     WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//img[@class=’component image-filmstrip']")))
+#
+# except TimeoutException:
+#     print("Timed out waiting for the page to load")
+#     browser.quit()
+
+
+max_recipe = 3
+recipe_count = 0
+output_dictionary = {}
 
 # Starting at a given webpage that includes others,
 # collects the urls, and moves to the new pages.
-Link_Elements = browser.find_elements_by_xpath("//a[@class='card__titleLink manual-link-behavior']")
-Links = []
-for i in range(0, len(Link_Elements), 2):
-    Links.append(Link_Elements[i].get_attribute('href'))
-for s in range(len(Links)):
-    browser.get(Links[s])
+link_elements = browser.find_elements_by_xpath("//a[@class='card__titleLink manual-link-behavior']")
+links = []
+for link_element in link_elements[::2]:
+    links.append(link_element.get_attribute('href'))
+
+for link in links:
+    browser.get(link)
+    print(link)
     # Pulls out recipe name
-    Recipe_Name_Elements = browser.find_element_by_xpath("//h1[@class='headline heading-content']")
-    Recipe_Name = Recipe_Name_Elements.text
-    print(Recipe_Name)
+
+    output_dictionary[link] = {}
+
+    recipe_name_elements = browser.find_element_by_xpath("//h1[@class='headline heading-content']")
+    recipe_name = recipe_name_elements.text
+
+    output_dictionary[link]["Recipe Name"] = recipe_name
+
+    print(recipe_name)
     print()
 
     # Pulls tags from the breadcrumb links, excluding those for Home and Recipe
-    Recipe_Tags_Elements = browser.find_elements_by_xpath("//span[@class='breadcrumbs__title']")
-    Recipe_Tag = [x.text for x in Recipe_Tags_Elements]
-    for q in range(2, len(Recipe_Tag)):
-        print(Recipe_Tag[q])
+    recipe_tags_elements = browser.find_elements_by_xpath("//span[@class='breadcrumbs__title']")
+    recipe_tags = [x.text for x in recipe_tags_elements]
+    for tag in recipe_tags[1:]:
+        print(tag)
     print()
+
+    output_dictionary[link]["Tags"] = recipe_tags[1:]
+
     # Searches through for any commas and cuts the string to exclude it
-    Recipe_Ingredients_Elements = browser.find_elements_by_xpath("//span[@class='ingredients-item-name']")
-    Recipe_Ingredients = [y.text for y in Recipe_Ingredients_Elements]
-    for c in range(len(Recipe_Ingredients)):
-        ingredient = Recipe_Ingredients[c]
-        for c2 in range(len(ingredient)):
-            if ingredient[c2] == ',':
-                Recipe_Ingredients[c] = ingredient[0:c2]
-    print(Recipe_Ingredients)
+    recipe_ingredients_elements = browser.find_elements_by_xpath("//span[@class='ingredients-item-name']")
+    recipe_ingredients = [y.text for y in recipe_ingredients_elements]
+    recipe_ingredients = list(map(lambda ingredient: ingredient.split(",")[0], recipe_ingredients))
+    print(recipe_ingredients)
     print()
+
+    output_dictionary[link]["Ingredients"] = recipe_ingredients
 
 
     # Looks for instructions and instruction steps in order
-    Steps = browser.find_elements_by_xpath("//li[@class='subcontainer instructions-section-item']//p")
-    Step_Text = [l2.text for l2 in Steps]
-    print(Step_Text)
+    steps = browser.find_elements_by_xpath("//li[@class='subcontainer instructions-section-item']//p")
+    step_text = [l2.text for l2 in steps]
+    print(step_text)
     print()
 
-    CookTime_Element = browser.find_elements_by_xpath("//div[@class='recipe-meta-item']")
-    CookTime = [t.text for t in CookTime_Element]
-    for v in range(len(CookTime)):
-        for n in range(len(CookTime[v])):
-            CookTime[v] = re.sub("\n", " ", CookTime[v])
-    print(CookTime)
-    print()
-try:
-    WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//img[@class=’component image-filmstrip']")))
+    output_dictionary[link]["Steps"] = step_text
 
-except TimeoutException:
-    print("Timed out waiting for the page to load")
-    browser.quit()
+    meta_info_element = browser.find_elements_by_xpath("//div[@class='recipe-meta-item']")
+    meta_info_list = [t.text for t in meta_info_element]
+
+    for meta_info in meta_info_list:
+        meta_type, meta_value = meta_info.split(":\n")
+        print(f"{meta_type}: {meta_value}")
+        print()
+        output_dictionary[link][meta_type] = meta_value
+
+    recipe_count += 1
+    if recipe_count >= max_recipe:
+        break
+
+with open(file_output_location,"w") as f:
+    json.dump(output_dictionary,f)
+
+
+
+
+
